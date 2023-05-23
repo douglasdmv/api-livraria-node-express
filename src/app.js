5c67ab61f9790b74443c141a1dc6cf17165167a9
Douglas Viegas <54741486+douglasdmv@users.noreply.github.com>
2023-05-22 21:21:09 -0300

    refatorando para padroes eslint

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ import express from "express"
 import db from "./config/dbConnect.js"
 import routes from "./routes/index.js"
 
-db.on("error", console.log.bind(console, 'Erro de conexao'))
+db.on("error", console.log.bind(console, "Erro de conexao"))
 db.once("open", () => {
-    console.log('Conexao feita com sucesso')
+  console.log("Conexao feita com sucesso")
 })
 
 const app = express()
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,15 +3,15 @@ import livros from "./livrosRoutes.js"
 import autores from "./autoresRoutes.js"
 
 const routes = (app) => {
-    app.route('/').get((req, res) => {
-        res.status(200).send({titulo: "Curso de Node"})
-    })
+  app.route("/").get((req, res) => {
+    res.status(200).send({titulo: "Curso de Node"})
+  })
 
-    app.use(
-        express.json(),
-        livros,
-        autores
-    )
+  app.use(
+    express.json(),
+    livros,
+    autores
+  )
 }
 
 export default routes
